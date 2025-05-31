942f06847d2af97d08b7e05497c10d436a3b361b
Damilola <Damilola2554@gmail.com>
2025-06-01 00:11:56 +0100

    added Js to the category so I can switch at will

---
+++ app.js
@@ -0,0 +1,23 @@
+const buttons = document.querySelectorAll(".button-group button");
+const items = document.querySelectorAll(".product-card");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const filter = button.getAttribute("data-filter");
+
+    buttons.forEach((btn) => btn.classList.remove("active"));
+    button.classList.add("active");
+
+    items.forEach((item) => {
+      if (filter === "all") {
+        item.classList.remove("hidden");
+      } else {
+        if (item.classList.contains(filter)) {
+          item.classList.remove("hidden");
+        } else {
+          item.classList.add("hidden");
+        }
+      }
+    });
+  });
+});
